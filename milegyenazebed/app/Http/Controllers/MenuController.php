d1926cac2a448134e24f16ea5f4629d98f078db8
szabomidu <>
2021-02-22 10:13:18 +0100

    Created MenuController with method getMenuDateFromWebsite to get the current menu date from the webpage and method saveMenuDateToDatabaseReturning id to create a new menu with that date and return id

--- milegyenazebed/app/Http/Controllers/MenuController.php
+++ milegyenazebed/app/Http/Controllers/MenuController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Menu;
+use Goutte\Client;
 
 class MenuController extends Controller
 {
-    //
+    private $client;
+    private $crawler;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+        $this->crawler = $this->client->request('GET', "http://arco-s.com/");
+    }
+
+    public function getMenuDateFromWebsite(): array
+    {
+        return $this->crawler->filter('.section-title')->each(function ($node) {
+            if ($node->text() != "Nyitvatartás" && $node->text() != "Asztalfoglalás") {
+                return $node->text();
+            }
+        });
+    }
+
+
+    public function saveMenuDateToDatabaseReturninId($date)
+    {
+        $menu = Menu::where('date', $date)
+            ->get();
+        if(!$menu){
+            $menu = Menu::create([
+                'date' => $date
+            ]);
+        }
+        return $menu->id;
+    }
 }

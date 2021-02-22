2a9efa9777b65457da890490c43879fe83b41825
szabomidu <>
2021-02-22 10:12:18 +0100

    Added an hourly schedule call to MenuController and DishControllers methods

--- milegyenazebed/app/Console/Kernel.php
+++ milegyenazebed/app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\DishController;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -19,12 +21,19 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $menuController = new MenuController();
+            $dishController = new DishController();
+            $menuData = $dishController->getMenuFromWebsite();
+            $date = $menuController->getMenuDateFromWebsite();
+            $menuId = $menuController->saveMenuDateToDatabaseReturninId($date);
+            $dishController->saveMenuToDatabase($menuData, $menuId);
+        })->hourly();
     }
 
     /**
@@ -34,7 +43,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }

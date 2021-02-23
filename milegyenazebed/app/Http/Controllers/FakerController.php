10c535f7e826e1658f549d40b4de823d9c1ae488
szabomidu <>
2021-02-23 11:01:07 +0100

    Refactored fake data creating

--- milegyenazebed/app/Http/Controllers/FakerController.php
+++ milegyenazebed/app/Http/Controllers/FakerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Dishes;
+use App\Models\Menu;
 use Faker\Factory;
 use FakerRestaurant\Provider\en_US\Restaurant;
 use Illuminate\Support\Facades\DB;
@@ -14,15 +15,14 @@ class FakerController extends Controller
         $faker = Factory::create();
         $faker->addProvider(new Restaurant($faker));
         foreach (range(1, 50) as $item) {
-            $id = DB::table('menu')
-            ->insertGetId([
-                'date' => $faker->date($format = 'Y.m.d', $max = 'now'),
+            $menu = Menu::create([
+                    'date' => $faker->dateTimeBetween("-1 years",'now')->format('Y.m.d'),
             ]);
             foreach(range(1,25) as $a) {
                 Dishes::create([
                     'dish_name' => $faker->foodName(),
                     'is_new' => $faker->boolean,
-                    'menu_id' => $id,
+                    'menu_id' => $menu->id,
                 ]);
             }
         }
